f30f3c8c716635731ccc0d841647bf7d1ce1e592
a-dera <53271706+a-dera@users.noreply.github.com>
2019-11-15 15:25:43 +0000

    Update main.py
    Fixes01

--- main.py
+++ main.py
@@ -1,62 +1,64 @@
 # Le jeu Pierre Papier Ciseau
 import os
 from random import *
+count=0#initialisation de notre compteur
+for  count in range(2,6) :#le jeu recommence 5 fois pas plus!
+    def choice ():
+        bot = randrange(3)
+        if bot == 0 :
+            if choix == 0:
+                print("Nous sommes à égalité")
+                #le jeu recommence
+            elif choix == 1:
+                print("Tu as choisi papier")
+                print("Et moi pierre")
+                print("Tu as gagné")
+            elif choix == 2:
+                print("Tu as choisi ciseau")
+                print("Et moi pierre")
+                print("J'ai gagné")
 
-def choice ():
-    bot = randrange(3)
-    if bot == 0 :
-        if choix == 0:
-            print("Nous sommes à égalité")
-            #le jeeu recommence
-        elif choix == 1:
-            print("Tu as choisi papier")
-            print("Et moi pierre")
-            print("Tu as gagné")
-        elif choix == 2:
-            print("Tu as choisi ciseau")
-            print("Et moi pierre")
-            print("J'ai gagné")
+        elif bot == 1 :
+            if choix == 1:
+                print("Nous sommes à égalité")
+                #le jeeu recommence
+            elif choix == 0:
+                print("Tu as choisi pierre")
+                print("Et moi papier")
+                print("Tu as perdu")
+            elif choix == 2:
+                print("Tu as choisi ciseau")
+                print("Et moi papier")
+                print("Tu as gagné")
 
-    elif bot == 1 :
-        if choix == 1:
-            print("Nous sommes à égalité")
-            #le jeeu recommence
-        elif choix == 0:
-            print("Tu as choisi pierre")
-            print("Et moi papier")
-            print("Tu as perdu")
-        elif choix == 2:
-            print("Tu as choisi ciseau")
-            print("Et moi papier")
-            print("Tu as gagné")
-
-    elif bot == 2 :
-        if choix == 2:
-            print("Nous sommes à égalité")
-            #le jeeu recommence
-        elif choix == 0:
-            print("Tu as choisi pierre")
-            print("Et moi ciseau")
-            print("Tu as gagné")
-        elif choix == 1:
-            print("Tu as choisi papier")
-            print("Et moi ciseau")
-            print("Tu as perdu")
-
-
-print("Pierre Papier Ciseau")
-print("Faites votre choix en entrant le chiffre correspondant (Veuillez choisir un chiffre valide)")
-print("0-> Pierre")
-print("1-> Papier")
-print("2-> Ciseau")
+        elif bot == 2 :
+            if choix == 2:
+                print("Nous sommes à égalité")
+                #le jeeu recommence
+            elif choix == 0:
+                print("Tu as choisi pierre")
+                print("Et moi ciseau")
+                print("Tu as gagné")
+            elif choix == 1:
+                print("Tu as choisi papier")
+                print("Et moi ciseau")
+                print("Tu as perdu")
 
 
-choix = int(input("Entrer votre choix :"))
-choice()
+    print("Pierre Papier Ciseau")
+    print("Faites votre choix en entrant le chiffre correspondant")
+    print("0-> Pierre")
+    print("1-> Papier")
+    print("2-> Ciseau")
+
+    #count=1#initialisation de notre compteur
+    choix = int(input("Entrer votre choix :"))
+    choice()
+    if count>1 and count<5:
+        print("-----------------On recommence pour la " ,count," ème fois-----------------")
+    if count==5:
+        print("------------------Nous sommes à notre 5ème reprise-----------------")
+        print("------------------On arrête là-------------------------------------")
 os.system("pause")
-cont = int(input("Pour continuer taper 1, sinon taper 0 :-> "))
-#while
-if cont == 1 :
-    import PPC.py
-else :
-    os.system("pause")
+#count = int(input("Pour continuer taper 1, sinon taper 0 :-> "))
+#count +=1# adaptation
